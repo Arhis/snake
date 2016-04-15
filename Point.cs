553f4887f795d53bf6ef5dc1fd3d0f8be1d50a4e
Arhis <avanture_boy@mail.ru>
2016-04-15 11:23:39 +0300

    Класс Point

---
+++ Point.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Виктор
+ * Date: 15.04.2016
+ * Time: 10:49
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace snake
+{
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	class Point
+	{
+		public int x;
+		public int y;
+		public char sym;
+		
+		public void Draw () {
+			Console.SetCursorPosition(x,y);
+			Console.Write(sym);
+		}
+	}
+}
