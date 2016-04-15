59a4cfa6a6229b2ee2eec79b1a30ef6d75788d78
Arhis <avanture_boy@mail.ru>
2016-04-15 06:24:05 +0300

    Hello world

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Виктор
+ * Date: 15.04.2016
+ * Time: 6:18
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace snake
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");			
+			Console.ReadLine();
+		}
+	}
+}
