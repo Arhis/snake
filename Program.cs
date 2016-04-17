1d54be6e91aa60ab75b6b766bd76c6dda34d7bd9
Arhis <avanture_boy@mail.ru>
2016-04-17 19:48:18 +0300

    Добыть еду, уйти от припятствий

--- Program.cs
+++ Program.cs
@@ -15,52 +15,48 @@ using System.Threading.Tasks;
 
 namespace snake
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{	
-			//Задание экранному пространству определенного размера
-			Console.SetBufferSize(80,25);
-			
-			// Отрисовка линий
-				HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');				
-				HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-				VerticalLine leftLine=new VerticalLine(0, 24, 0, '+');
-				VerticalLine rightLine=new VerticalLine(0, 24, 78, '+');
-			 	upLine.Drow();
-			 	downLine.Drow();
-				leftLine.Drow();
-				rightLine.Drow();
-				
-			//Отрисовка точек
-			Point p= new Point(4, 5, '*');
-			Snake snake=new Snake(p, 4, Direction.RIGHT);
-			snake.Drow();
-			
-			FoodCreator foodCreator = new FoodCreator( 80, 25, '$' );
-			Point food = foodCreator.CreateFood();
-			food.Draw();
-			
-			while(true)
-			{
-				if(snake.Eat( food ) )
-				{
-					food = foodCreator.CreateFood();
-					food.Draw();
-				}
-				else
-				{
-					snake.Move();
-				}					
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetBufferSize(80, 25);
 
-				Thread.Sleep( 100 );
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
-				if (Console.KeyAvailable)
-				{
-					ConsoleKeyInfo key = Console.ReadKey();
-					snake.HandleKey( key.Key );
-				}
-			}					
-		}
-	}
+            // Отрисовка точек			
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Draw();
+
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
+            {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+                if (snake.Eat(food))
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+                if (Console.KeyAvailable)
+                {
+                    ConsoleKeyInfo key = Console.ReadKey();
+                    snake.HandleKey(key.Key);
+                }
+            }
+        }
+
+    }
 }
